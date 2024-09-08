c8b6c8feb7d86c564023165442b768327edd73b9
Sudhanshu-Devmorari <122085757+Sudhanshu-Devmorari@users.noreply.github.com>
2024-09-08 18:02:02 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    description = db.Column(db.String(200))
+    user = db.relationship('User', back_populates='transactions')
+
+User.transactions = db.relationship('Transaction', order_by=Transaction.id, back_populates='user')
